6d598bb55d6eb4f8a9850af1829e9324e8ebb891
Larson007 <mohamed.benallal.pro@gmail.com>
2023-10-28 19:04:51 +0200

    Remove homepage lorem

--- views/homepage/index.php
+++ views/homepage/index.php
@@ -5,11 +5,11 @@
 <?php endif ?>
 <div class="globe-animation">
     <p class="globe-animation--text">TimeLines est une plateforme communautaire éducative utilsant des frises chronologiques en support visuel</p>
-    <div class="bounce">
+    <!-- <div class="bounce">
         <a href="#homepage"><i class="fa-solid fa-angle-down"></i></a>
-    </div>
+    </div> -->
 </div>
-
+<!-- 
 <div id="homepage">
     <h1 class="homepage--title">Bienvenue</h1>
     <div class="homepage--texte">
@@ -26,4 +26,4 @@
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum ut asperiores quae quaerat ipsam quasi ex, sapiente odit suscipit, cupiditate, iusto minima temporibus animi error itaque sit porro quod? Deserunt consectetur modi quibusdam soluta ratione, sequi repellat maxime a? Corporis ut minus deleniti maxime. Deserunt dolorum laudantium cum quae fuga!</p>
         </div>
     </div>
-</div>
+</div> -->
